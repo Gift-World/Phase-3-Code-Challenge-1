ff333515fbf214519ab8ef64ed3801a1aa7173c5
charles <charles.angila@student.moringaschool.com>
2024-09-01 21:46:41 +0300

    added function merge_dicts that  takes two dictionaries as input and merges them into a single dictionary. If there are any common keys, their values should be summed up

--- task8.py
+++ task8.py
@@ -1,2 +1,11 @@
-def merge_dicts(d1,d2):
-    
+def merge_dicts(d1, d2):
+    merged_dict = d1.copy()
+
+    for key, value in d2.items():
+        if key in merged_dict:
+            merged_dict[key] += value
+        else:
+            merged_dict[key] = value
+
+    return merged_dict
+
