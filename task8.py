7c6484583c0b34c2457fe9db67a73f8ff88ad8b7
charles <charles.angila@student.moringaschool.com>
2024-09-03 09:06:30 +0300

    updated task 8

--- task8.py
+++ task8.py
@@ -1,11 +1,11 @@
 def merge_dicts(d1, d2):
-    merged_dict = d1.copy()
+    merged_dictionary = d1.copy()
 
     for key, value in d2.items():
-        if key in merged_dict:
-            merged_dict[key] += value
+        if key in merged_dictionary:
+            merged_dictionary[key] += value
         else:
-            merged_dict[key] = value
+            merged_dictionary[key] = value
 
-    return merged_dict
+    return merged_dictionary
 
